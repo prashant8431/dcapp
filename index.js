b9cc8298ef5bef22563fd5231214c13ff2262d74
Prashant Mohite <prashantmohite409@gmail.com>
2021-04-07 12:02:31 +0530

    Dc app

---
+++ index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "./fonts/Montserrat/Montserrat-Light.ttf";
+
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost/dcAppBack/public/api";
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
